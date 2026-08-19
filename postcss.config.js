42f9aaf57711c09449273d22cd78e90954e86e4f
Sadia Ferdous <sadiaferdous003@gmail.com>
2026-08-19 09:11:21 -0400

    Add sunset photos to portfolio

--- postcss.config.js
+++ postcss.config.js
@@ -2,4 +2,4 @@ module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
   },
-};
+};https://azwonders.com/2024/01/09/check-out-this-epic-i-sunset-captured-in-the-phoenix-mountains-preserve/https://www.google.com/imgres?q=sunset&imgurl=https%3A%2F%2Fi0.wp.com%2Fazwonders.com%2Fwp-content%2Fuploads%2F2023%2F12%2Fepic-sunset-3.jpg%3Fresize%3D576%252C1024%26ssl%3D1&imgrefurl=https%3A%2F%2Fazwonders.com%2F2024%2F01%2F09%2Fcheck-out-this-epic-i-sunset-captured-in-the-phoenix-mountains-preserve%2F&docid=SzGclrqNJcVJ7M&tbnid=eFVmkRV5Nh48oM&vet=12ahUKEwjW3Y244ayWAxUckYkEHRbvJD8QnPAOegQIIRAA..i&w=576&h=1024&hcb=2&ved=2ahUKEwjW3Y244ayWAxUckYkEHRbvJD8QnPAOegQIIRAA
